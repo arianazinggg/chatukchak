d5013aedc8aadc610fee85a0ec659271eb357e3c
arianazinggg <110186183+arianazinggg@users.noreply.github.com>
2023-11-18 20:11:39 +0800

    Done with clicking login

--- app/src/main/java/com/example/chatukchak/LoginActivity.kt
+++ app/src/main/java/com/example/chatukchak/LoginActivity.kt
@@ -2,10 +2,22 @@ package com.example.chatukchak
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.content.Intent
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var btnSignUp: Button
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnSignUp = findViewById(R.id.button5)
+        btnSignUp.setOnClickListener {
+            Log.d("ZZZTEST", "Clicked Login")
+            var nextActivity: Intent
+            nextActivity = Intent(this, MainActivity::class.java)
+            startActivity(nextActivity)
+        }
     }
 }
