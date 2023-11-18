8ecf848ba66673fcd4bccde947efb35c7cc8adce
arianazinggg <110186183+arianazinggg@users.noreply.github.com>
2023-11-18 22:46:50 +0800

    almost firebase

--- app/src/main/java/com/example/chatukchak/LoginActivity.kt
+++ app/src/main/java/com/example/chatukchak/LoginActivity.kt
@@ -5,24 +5,50 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.content.Intent
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity: AppCompatActivity() {
+
+    lateinit var mAuth: FirebaseAuth
+
+    lateinit var btnLogin: Button
+    lateinit var btnRegister: Button
+
+    lateinit var etEmail: EditText
+    lateinit var etPassw: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
-        lateinit var btnLogin: Button
-        lateinit var btnRegister: Button
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        mAuth = FirebaseAuth.getInstance()
+
         Log.d("ZZZTEST", "Called on Create")
         btnLogin = findViewById(R.id.button5)
+        btnRegister = findViewById(R.id.button6)
+        etEmail = findViewById(R.id.loginemail)
+        etPassw = findViewById(R.id.loginpassw)
         btnLogin.setOnClickListener {
+
+            var val_email = etEmail.text.toString()
+            var val_passw = etPassw.text.toString()
+
+            if(val_email.equals("")||val_passw.equals("")){
+                Toast.makeText(this, "Required fields", Toast.LENGTH_SHORT)
+
+            }else{
+
+            }
+
+
             Log.d("ZZZTEST", "Clicked Signup")
             var nextActivity: Intent
             nextActivity = Intent(this, MainActivity::class.java)
             startActivity(nextActivity)
 
         }
-        btnRegister = findViewById(R.id.button6)
         btnRegister.setOnClickListener {
         Log.d("ZZZTEST", "Clicked Register")
         val regis: Intent
@@ -30,4 +56,6 @@ class LoginActivity : AppCompatActivity() {
         startActivity(regis)
         }
     }
+
+    fun_sign
 }
---
+++ app/src/main/java/com/example/chatukchak/services/FirebaseLib.kt
@@ -0,0 +1,5 @@
+package com.example.chatukchak.services
+
+class FirebaseLib {
+
+}
